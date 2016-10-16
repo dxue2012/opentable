d42c4686d830189b7f7710b52505f42d78f9dac9
Dyland Xue <dylandxue2012@gmail.com>
2016-10-16 17:41:09 -0400

    more work

--- opentable.py
+++ opentable.py
@@ -9,9 +9,9 @@ def date_to_string(date):
 def extract_time(datetime_str):
     return datetime.datetime.strptime(datetime_str, "%Y-%m-%d %H:%M").strftime('%H:%M')
 
-def post_request(date):
+def post_request(rid, date):
     # CR dxue for dxue: hard-code nakazawa's sushi bar for now. Make this configurable
-    url = "http://www.opentable.com/restaurant/profile/118903/search"
+    url = "http://www.opentable.com/restaurant/profile/" + rid + "/search"
     dateTime = date_to_string(date) + " 19:00"
     payload = {"covers" : 2, "dateTime": dateTime}
     return requests.post(url, data=payload)
@@ -28,19 +28,34 @@ def parse_response(response):
         available_datetimes = [button["data-datetime"] for button in all_buttons if button not in unavailable_buttons]
         return [extract_time(x) for x in available_datetimes]
 
-def available_times_on(date):
-    return parse_response(post_request(date))
+def available_times(rid, date):
+    return parse_response(post_request(rid, date))
 
 def has_availability_on(date):
     return len(available_times_on(date)) == 0
 
+def get_restaurant_rid(restaurant_str):
+    url = "http://www.opentable.com/" + restaurant_str
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    try:
+        return soup.find('a', class_='favorite-button')['data-rid']
+    except Exception:
+        return None
+
 def main():
     restaurant = sys.argv[1]
-    today = datetime.date.today()
-    for x in range(30):
-        date = today + datetime.timedelta(days=x)
-        avail = available_times_on(date)
-        print len(avail)
-        print "The restaurant " + restaurant + " has availability on " + date_to_string(date) + "? " + str(avail)
+    rid = get_restaurant_rid(restaurant)
+    if rid is None:
+        print restaurant + " not found. Make sure the url http://www.opentable.com/RESTAURANT is valid."
+        exit(1)
+    else:
+        today = datetime.date.today()
+        print "looking for available for " + restaurant + " in the next 30 days..."
+        for x in range(30):
+            date = today + datetime.timedelta(days=x)
+            avail = available_times(rid, date)
+            print "has availability on " + date_to_string(date) + "? " + str(avail)
+        print "Done!"
 
 main()
